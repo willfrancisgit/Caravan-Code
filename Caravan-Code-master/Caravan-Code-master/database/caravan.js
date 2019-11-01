515433f54f50690a3b11273cc4df5f21abaa16ac
sean-mceachern <50753728+sean-mceachern@users.noreply.github.com>
2019-11-01 15:33:23 -0600

    Add files via upload
    reorganizing folders

---
+++ Caravan-Code-master/Caravan-Code-master/database/caravan.js
@@ -0,0 +1,66 @@
+/***********************
+ 
+  Load Components!
+
+  Express      - A Node.js Framework
+  Body-Parser  - A tool to help use parse the data in a post request
+  Pug          - A view engine for dynamically rendering HTML pages
+  Pg-Promise   - A database tool to help use connect to our PostgreSQL database
+
+***********************/
+
+const express = require('express'); // Add the express framework has been added
+let app = express();
+
+const bodyParser = require('body-parser'); // Add the body-parser tool has been added
+app.use(bodyParser.json());              // Add support for JSON encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // Add support for URL encoded bodies
+
+const pug = require('pug'); // Add the 'pug' view engine
+
+//Create Database Connection
+const pgp = require('pg-promise')();
+
+
+/**********************
+  
+  Database Connection information
+
+  host: This defines the ip address of the server hosting our database.  We'll be using localhost and run our database on our local machine (i.e. can't be access via the Internet)
+  port: This defines what port we can expect to communicate to our database.  We'll use 5432 to talk with PostgreSQL
+  database: This is the name of our specific database.  From our previous lab, we created the football_db database, which holds our football data tables
+  user: This should be left as postgres, the default user account created when PostgreSQL was installed
+  password: This the password for accessing the database.  You'll need to set a password USING THE PSQL TERMINAL THIS IS NOT A PASSWORD FOR POSTGRES USER ACCOUNT IN LINUX!
+
+**********************/
+// REMEMBER to chage the password
+
+const dbConfig = {
+	host: 'ec2-54-83-201-84.compute-1.amazonaws.com',
+	port: 5432,
+	database: 'd2a5sb6n9aknkq',
+	user: 'hqgxkojofdagea',
+	password: '0af0023a8d186e31990e1a1eee7c8ba7ca84f35194ff2785e46aaf9d340920db'
+};
+
+let db = pgp(dbConfig);
+
+// set the view engine to ejs
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/')); // This line is necessary for us to use relative paths and access our resources directory
+
+//profile
+app.get('/profile.html',function(req, res))
+{
+  res.render
+}
+
+
+
+
+
+
+
+
+
+
